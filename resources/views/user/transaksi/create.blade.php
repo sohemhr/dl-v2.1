ab6c3c820826cb92542af2c7c991f357c4a7000e
sohemhr <sohe.mhr@gmail.com>
2025-06-12 11:15:26 +0700

    modified transaksi

--- resources/views/user/transaksi/create.blade.php
+++ resources/views/user/transaksi/create.blade.php
@@ -1,11 +1,6 @@
-@props(['title', 'subtitle'])
-<!DOCTYPE html>
-<html lang="id">
-    <x-layouts.head title="{{ $title }}" subtitle="{{ $subtitle }}" />
-    <x-layouts.navbar title="{{ $title }}" subtitle="{{ $subtitle }}" />
-    <x-layouts.sidebar title="{{ $title }}" subtitle="{{ $subtitle }}" />
-    <x-layouts.header title="{{ $title }}" subtitle="{{ $subtitle }}" />
+@extends('components.layouts.app')
 
+@section('content')
     <!-- Main content here -->
     <section class="content">
         <div class="container-fluid">
@@ -316,9 +311,7 @@
 
     </section>
     <!-- /.content -->  
-
-    <x-layouts.footer />
-    <x-layouts.js />
+@endsection
     {{-- js new star here --}}
     <script>   
         $(function () {
