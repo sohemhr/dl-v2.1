23ce3d82a8a20a14063e37d438138fc963c25356
sohemhr <sohe.mhr@gmail.com>
2025-06-12 11:07:24 +0700

    modified perusahaan

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -14,44 +14,44 @@
             <li class="menu-title small text-uppercase">
                 <span class="menu-title-text">MAIN</span>
             </li>
-            <li class="menu-item open">
+            <li class="menu-item">
                 <a href="/admstr/dashboard" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">dashboard</span>
                     <span class="title">Dashboard</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/kategori" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/kategori" class="menu-link {{ Request::is('admstr/kategori') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">category</span>
                     <span class="title">Kategori</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link {{ Request::is('admstr/artikel') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">Article</span>
                     <span class="title">Data Artikel</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/promo" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/promo" class="menu-link {{ Request::is('admstr/promo') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">Campaign</span>
                     <span class="title">Data Promo</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/career" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/career" class="menu-link {{ Request::is('admstr/career') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">Work</span>
                     <span class="title">Data Career</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/contact" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/contact" class="menu-link {{ Request::is('admstr/contact') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">Contacts</span>
                     <span class="title">Contact</span>
                 </a>
             </li>
-             <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/checking2" class="menu-link hover:bg-blue-500">
+             <li class="menu-item open">
+                <a href="/admstr/checking2" class="menu-link {{ Request::is('admstr/checking2') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">Search_Check</span>
                     <span class="title">Checking</span>
                 </a>
@@ -59,47 +59,47 @@
             <li class="menu-title small text-uppercase">
                 <span class="menu-title-text">TRANSAKSI</span>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/perusahaan" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/perusahaan" class="menu-link {{ Request::is('admstr/perusahaan') ? 'active' : '' }}">
                     <span class="material-symbols-outlined menu-icon">patient_list</span>
                     <span class="title">Data Perusahaan</span>
                 </a>
             </li>
             <li class="menu-item">
-                <a href="/admstr/artikel" class="menu-link menu-toggle active hover:bg-blue-500">
+                <a href="/admstr/artikel" class="menu-link menu-toggle active">
                     <span class="material-symbols-outlined menu-icon">receipt</span>
                     <span class="title">Data Transaksi</span>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
-                        <a href="/admstr/transaksi" class="menu-link hover:bg-blue-500">
+                        <a href="/admstr/transaksi" class="menu-link">
                             <span class="title">Transaksi</span>
                         </a>
                     </li>
                     <li class="menu-item">
-                        <a href="/admstr/transaksi" class="menu-link hover:bg-blue-500">
+                        <a href="/admstr/transaksi" class="menu-link">
                             <span class="title">Start</span>
                         </a>
                     </li>
                     <li class="menu-item">
-                        <a href="/admstr/transaksi" class="menu-link hover:bg-blue-500">
+                        <a href="/admstr/transaksi" class="menu-link">
                             <span class="title">On Process</span>
                         </a>
                     </li>
                     <li class="menu-item">
-                        <a href="/admstr/transaksi" class="menu-link hover:bg-blue-500">
+                        <a href="/admstr/transaksi" class="menu-link">
                             <span class="title">Pending</span>
                         </a>
                     </li>
                     <li class="menu-item">
-                        <a href="/admstr/transaksi" class="menu-link hover:bg-blue-500">
+                        <a href="/admstr/transaksi" class="menu-link">
                             <span class="title">Completed</span>
                         </a>
                     </li>
                 </ul>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">payments</span>
                     <span class="title">Data Pembayaran</span>
                 </a>
@@ -107,44 +107,44 @@
             <li class="menu-title small text-uppercase">
                 <span class="menu-title-text">MASTER WEB</span>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link>
                     <span class="material-symbols-outlined menu-icon">Domain</span>
                     <span class="title">Office</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">credit_card</span>
                     <span class="title">Rekening</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">feature_search</span>
                     <span class="title">Layanan</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">Search_Check</span>
                     <span class="title">Include List</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">List</span>
                     <span class="title">List Proses</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">Database</span>
                     <span class="title">Backup Database</span>
                 </a>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">construction</span>
                     <span class="title">Maintenance</span>
                 </a>
@@ -152,8 +152,8 @@
               <li class="menu-title small text-uppercase">
                 <span class="menu-title-text">USER WEB</span>
             </li>
-            <li class="menu-item open hover:bg-blue-500">
-                <a href="/admstr/artikel" class="menu-link hover:bg-blue-500">
+            <li class="menu-item open">
+                <a href="/admstr/artikel" class="menu-link">
                     <span class="material-symbols-outlined menu-icon">group</span>
                     <span class="title">Users</span>
                 </a>
--- resources/views/user/perusahaan/index.blade.php
+++ resources/views/user/perusahaan/index.blade.php
@@ -1,43 +1,38 @@
-@props(['title', 'subtitle'])
-<!DOCTYPE html>
-<html lang="id">
-    <x-layouts.head title="{{ $title }}" subtitle="{{ $subtitle }}" />
-    <x-layouts.navbar title="{{ $title }}" subtitle="{{ $subtitle }}" />
-    <x-layouts.sidebar title="{{ $title }}" subtitle="{{ $subtitle }}" />
-    <x-layouts.header title="{{ $title }}" subtitle="{{ $subtitle }}" />
+@extends('components.layouts.app')
 
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        @if (session('success'))
+@section('content')
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    @if (session('success'))
                         <div class="alert alert-success alert-dismissible">
                             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
                             <h5>
-                                <i class="icon fas fa-check"></i> 
+                                <i class="icon fas fa-check"></i>
                                 {{ session('success') }}
                             </h5>
                         </div>
-                        @elseif (session('delete'))
+                    @elseif (session('delete'))
                         <div class="alert alert-danger alert-dismissible">
                             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
                             <h5><i class="icon fas fa-check"></i> {{ session('delete') }}</h5>
                         </div>
-                        @endif
-                        <div class="card bg-slate-200">                
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                    <a href="/admstr/perusahaan/create">
+                    @endif
+                    <div class="card bg-slate-200">
+                        <div class="card-header">
+                            <h3 class="card-title">
+                                <a href="/admstr/perusahaan/create">
                                     <button type="button" class="btn btn-primary">
                                         <i class="fa fa-plus" aria-hidden="true"></i>
                                         &nbsp;Tambah Data
                                     </button>
-                                    </a>
-                                </h3>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body">
+                                </a>
+                            </h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
                             <table id="example2" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
@@ -48,110 +43,101 @@
                                         <th>No.Hp. An</th>
                                         <th>Tanggal</th>
                                         <th>Pic</th>
-                                        <th>Action</th>            
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                @php
-                                    $no = 1;
-                                @endphp
-                                @foreach ($getPerusahaan as $item)
-                                    <tr>
-                                        <td>{{ $no++ }}</td>
-                                        <td>{{ $item->perusahaan_kode }}</td>
-                                        <td>{{ $item->perusahaan_nama }}</td>
-                                        <td>{{ $item->perusahaan_atas_nama }}</td>
-                                        <td>{{ $item->perusahaan_an_hp }}</td>
-                                        <td>{{ \Carbon\Carbon::parse($item->perusahaan_tgl)->format('d-m-Y') }}</td>
-                                        <td>{{ $item->nama }}</td>                            
-                                        <td>
-                                            <a href="/admstr/perusahaan/details/{{ $item->perusahaan_uuid }}">
-                                                <button type="button" class="btn btn-xs btn-warning">
-                                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                                    &nbsp;
+                                    @php
+                                        $no = 1;
+                                    @endphp
+                                    @foreach ($getPerusahaan as $item)
+                                        <tr>
+                                            <td>{{ $no++ }}</td>
+                                            <td>{{ $item->perusahaan_kode }}</td>
+                                            <td>{{ $item->perusahaan_nama }}</td>
+                                            <td>{{ $item->perusahaan_atas_nama }}</td>
+                                            <td>{{ $item->perusahaan_an_hp }}</td>
+                                            <td>{{ \Carbon\Carbon::parse($item->perusahaan_tgl)->format('d-m-Y') }}</td>
+                                            <td>{{ $item->nama }}</td>
+                                            <td>
+                                                <a href="/admstr/perusahaan/details/{{ $item->perusahaan_uuid }}">
+                                                    <button type="button" class="btn btn-xs btn-warning">
+                                                        <i class="fa fa-eye" aria-hidden="true"></i>
+                                                        &nbsp;
+                                                    </button>
+                                                </a>
+                                                @php
+                                                    $getTrxByKode = DB::table('transaksis')->where('trx_perusahaan_kode', $item->perusahaan_kode)->count();
+                                                @endphp
+                                                @if ($getTrxByKode > 0)
+
+                                                @else
+                                                    <a href="/admstr/transaksi/create/{{ $item->perusahaan_uuid }}">
+                                                        <button type="button" class="btn btn-xs btn-primary">
+                                                            <i class="fa fa-cogs" aria-hidden="true"></i>
+                                                            &nbsp;TrxAdd
+                                                        </button>
+                                                    </a>
+                                                @endif
+
+                                                <a href="/admstr/perusahaan/show/{{ $item->perusahaan_uuid }}">
+                                                    <button type="button" class="btn btn-xs btn-success">
+                                                        <i class="fa fa-edit" aria-hidden="true"></i>
+                                                        &nbsp;
+                                                    </button>
+                                                </a>
+                                                <button type="button" class="btn btn-xs btn-danger" data-toggle="modal"
+                                                    data-target="#modal-delete{{ $item->perusahaan_uuid }}">
+                                                    <i class="fa fa-trash" aria-hidden="true"></i>
                                                 </button>
-                                            </a>
-                                            @php
-                                                $getTrxByKode = DB::table('transaksis')->where('trx_perusahaan_kode', $item->perusahaan_kode)->count();
-                                            @endphp
-                                            @if ($getTrxByKode > 0)
-                                                
-                                            @else
-                                                <a href="/admstr/transaksi/create/{{ $item->perusahaan_uuid }}">
-                                                <button type="button" class="btn btn-xs btn-primary">
-                                                    <i class="fa fa-cogs" aria-hidden="true"></i>
-                                                    &nbsp;TrxAdd  
-                                                </button>
-                                            </a>
-                                            @endif
-                                            
-                                            <a href="/admstr/perusahaan/show/{{ $item->perusahaan_uuid }}">
-                                                <button type="button" class="btn btn-xs btn-success">
-                                                    <i class="fa fa-edit" aria-hidden="true"></i>
-                                                    &nbsp;
-                                                </button>
-                                            </a>
-                                            <button type="button" class="btn btn-xs btn-danger" data-toggle="modal" data-target="#modal-delete{{ $item->perusahaan_uuid }}">
-                                            <i class="fa fa-trash" aria-hidden="true"></i>
-                                            </button> 
-                                        </td>                           
-                                    </tr> 
-                                @endforeach                   
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
-                            </div>
-                            <!-- /.card-body -->
                         </div>
-                        <!-- /.card -->
+                        <!-- /.card-body -->
                     </div>
-                    <!-- /.col -->
+                    <!-- /.card -->
                 </div>
-                <!-- /.row -->
+                <!-- /.col -->
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
 
 
-            @foreach ($getPerusahaan as $key)
+        @foreach ($getPerusahaan as $key)
             <div class="modal fade" id="modal-delete{{ $key->perusahaan_uuid }}">
-            <div class="modal-dialog">
-                <div class="modal-content bg-white">
-                <div class="modal-header bg-danger">
-                    <h4 class="modal-title">
-                    <i class="fa fa-trash" aria-hidden="true"></i>
-                    Hapus Data Include List
-                    </h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
+                <div class="modal-dialog">
+                    <div class="modal-content bg-white">
+                        <div class="modal-header bg-danger">
+                            <h4 class="modal-title">
+                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                Hapus Data Include List
+                            </h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Yakin Anda akan menghapus data {{ $key->perusahaan_nama }}&hellip;?</p>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <form action="/admstr/perusahaan/destroy/{{ $key->perusahaan_uuid }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Hapus</button>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
                 </div>
-                <div class="modal-body">
-                    <p>Yakin Anda akan menghapus data {{ $key->perusahaan_nama }}&hellip;?</p>
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <form action="/admstr/perusahaan/destroy/{{ $key->perusahaan_uuid }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Hapus</button>
-                    </form>                
-                </div>
-                </div>
-                <!-- /.modal-content -->
-            </div>
-            <!-- /.modal-dialog -->
+                <!-- /.modal-dialog -->
             </div>
             <!-- /.modal delete -->
-            @endforeach
-            
-
-
-        </section>
-        <!-- /.content -->  
-
-    <x-layouts.footer />
-    <x-layouts.js />
-    {{-- js new star here --}}
-
-    {{-- js new end --}}
-</body>
-</html>
+        @endforeach
+    </section>
+    <!-- /.content -->
+@endsection
